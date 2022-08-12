3e8cb98f734a157c1ccaab410b3e60b5291f2217
Hassam Ismail <haismail164@gmail.com>
2022-08-12 17:49:07 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+fetch("https://training12082022.herokuapp.com/")
+  .then((response) => response.json())
+  .then((json) => {
+    var students = document.getElementById("students");
+    json.Sheet1.forEach((element) => {
+      var option = document.createElement("option");
+      option.innerHTML = element.A;
+      students.appendChild(option);
+    });
+  });
